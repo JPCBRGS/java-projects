75997daee5c536213fc3a27f1a3071d6e660014b
João Pedro Borges <61915220+JPCBRGS@users.noreply.github.com>
2023-02-16 18:10:08 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Java_Studies
+Basic Java code I made while studying this language (and also some simple Netbeans projects).
---
+++ table_model_list/src/model/Produto.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author JP
+ */
+public class Produto {
+    
+    private String descricao;
+    private int quantidade;
+    private double valor;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int qtd) {
+        this.quantidade = qtd;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+    
+    
+    
+}
